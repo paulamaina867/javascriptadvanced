8ae6489626e2cded2d5dfc4cba4e43ce8d974488
paulamaina867 <mainapauline867@gmail.com>
2025-06-03 09:57:05 +0300

    updated code

---
+++ lesson4a.js
@@ -0,0 +1,22 @@
+//find the average of the three subjects
+let average = total / 3;
+console.log("The total is: " + total)
+console.log("The average is: " + average)
+if (attendance < 75){
+    console.log("Failed due to low attendance")
+}
+else if (average >= 90) {
+    console.log("Grade: A")
+}
+else if (average >= 80 && average <90){
+    console.log("Grade: B")
+}
+else if (average >= 70 && average < 80){
+    console.log("Grade: C")
+}
+else if (average >= 60 && average < 70){
+    console.log("Grade: D")
+}
+else{
+    console.log("Grade: E")
+}
